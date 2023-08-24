12970656f10ddc5eca8d29a663086401ffe9ca1f
Dhallyson Santos <39205621+dhallyson@users.noreply.github.com>
2023-08-24 20:50:53 -0300

    Update Atividade08.c

--- Atividade08.c
+++ Atividade08.c
@@ -1,38 +1,39 @@
-#include<stdio.h>
-#include<math.h>
+#include <math.h>
+#include <stdio.h>
 
-main(){
+main() {
 
-    /*
-    Escreva um algoritmo para ler o salário mensal atual de um funcionário
-    e o percentual de reajuste. Calcular e escrever o valor do novo salário.
-    */
+  /*
+  Escreva um algoritmo para ler o salário mensal atual de um funcionário
+  e o percentual de reajuste. Calcular e escrever o valor do novo salário.
+  */
 
-    // Definição de variáveis
-    float salarioMensal, salarioNovoValor;
-    int salarioPercentualReajuste;
+  // Definição de variáveis
+  float salarioMensal, salarioNovoValor;
+  int salarioPercentualReajuste;
 
-    // Scan salario Mensal
+  // Scan salario Mensal
 
-    printf("Digite seu salario mensal: ");
-    scanf("%f", &salarioMensal);
-    fflush(stdin);
+  printf("Digite seu salario mensal: ");
+  scanf("%f", &salarioMensal);
+  // fflush(stdin);
 
-    // Scan reajuste salarial
+  // Scan reajuste salarial
 
-    printf("Digite a porcentagem do reajuste: ");
-    scanf("%d", &salarioPercentualReajuste);
-    fflush(stdin);
+  printf("Digite a porcentagem do reajuste: ");
+  scanf("%d", &salarioPercentualReajuste);
+  // fflush(stdin);
 
-    // Processamento dos valores
+  // Processamento dos valores
 
-    // Calculo 01: Descobrir quanto vale a porcentagem do salário e somar ao salário
-    salarioNovoValor = ((salarioMensal / salarioPercentualReajuste) * 100) + salarioMensal;
+  // Calculo 01: Descobrir quanto vale a porcentagem do salário e somar ao
+  // salário
+  salarioNovoValor =
+      ((salarioPercentualReajuste * 0.01) * salarioMensal) + salarioMensal;
 
-    // Mostra os resultados
+  // Mostra os resultados
 
-    printf("O reajuste de %d em cima do salario de %f resulta no salario novo R$ %.2f", salarioPercentualReajuste, salarioMensal, salarioNovoValor);
-
-    
-
+  printf("O reajuste de %d em cima do salario de %.2f resulta no salario novo R$ "
+         "%.2f",
+         salarioPercentualReajuste, salarioMensal, salarioNovoValor);
 }
