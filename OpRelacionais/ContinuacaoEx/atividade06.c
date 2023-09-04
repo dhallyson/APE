ef55237f869d0b9820857a8d0c670448f58050e1
Dhallyson Santos <39205621+dhallyson@users.noreply.github.com>
2023-09-04 14:58:02 -0300

    Update atividade06.c

--- OpRelacionais/ContinuacaoEx/atividade06.c
+++ OpRelacionais/ContinuacaoEx/atividade06.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+main() {
+  /*
+  Codifique um programa que lê um número inteiro. A seguir o programa deve imprimir uma mensagem     para o usuário dizendo se o número digitado é par. Se o número não for par, o programa não deve    fazer nada.  
+  */
+
+  int number;
+
+  printf("Digite um número inteiro: ");
+  scanf("%d", &number);
+
+  if(number % 2 != 1){
+    printf("O número é par");
+  }
+}
