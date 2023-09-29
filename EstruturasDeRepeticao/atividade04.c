2ada9489fbe32c3d014557ae97a13e0090139312
Dhallyson Santos <39205621+dhallyson@users.noreply.github.com>
2023-09-29 10:18:29 -0300

    Update atividade04.c

--- EstruturasDeRepeticao/atividade04.c
+++ EstruturasDeRepeticao/atividade04.c
@@ -2,9 +2,24 @@
 
 main() {
 
-  int cont, num = 15;
+  // int cont, num = 15;
 
-  for(cont = 3; cont <= num; cont+=3){
-    printf("\n%d", cont);
+  // for(cont = 3; cont <= num; cont+=3){
+  //   printf("\n%d", cont);
+  // }
+
+
+  int qtdDesejada = 10, multiplo = 3, contador = 1, qtd = 1;
+
+  while (qtd <= qtdDesejada) {
+
+    if ((contador % multiplo) == 0) {
+      printf("\n%d", contador);
+      qtd++;
+    }
+
+    contador++;
   }
 }
+
+
