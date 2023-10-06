20b4ec70c02f88375473f4b11a1e2cb343dda047
Dhallyson Santos <39205621+dhallyson@users.noreply.github.com>
2023-10-06 10:28:33 -0300

    Create exercicio05.c

---
+++ vetores/exercicio05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main() {
+
+  int vetor[10], pares = 0;
+
+  for (int i = 0; i < 9; i++) {
+    printf("\nO valor do vetor[%d]: ", i);
+    scanf("%d", &vetor[i]);
+
+    if(vetor[i] % 2 == 0){
+      pares++;
+    }
+  }
+
+  printf("Existem %d pares", pares);
+}
