2b07bd48a331699aff0606d5f2befc576ec6f518
Dhallyson Santos <39205621+dhallyson@users.noreply.github.com>
2023-10-31 10:31:15 -0300

    Update exercicio05.c

--- IntroducaoFuncoes/exercicio05.c
+++ IntroducaoFuncoes/exercicio05.c
@@ -4,19 +4,22 @@
 double calcularVolumeEsfera(double raio);
 
 int main() {
-    double raio, volume;
+    double raio, volume, pi;
     
+    printf("Digite o valor de pi: ");
+    scanf("%lf", &pi);
+
     printf("Digite o raio da esfera: ");
     scanf("%lf", &raio);
 
-    volume = calcularVolumeEsfera(raio);
+    volume = calcularVolumeEsfera(raio, pi);
 
     printf("O volume da esfera é %.2lf unidades cúbicas.\n", volume);
 
     return 0;
 }
 
-double calcularVolumeEsfera(double raio) {
-    double volume = (4.0 / 3.0) * M_PI * pow(raio, 3);
+double calcularVolumeEsfera(double raio, double pi) {
+    double volume = (4.0 / 3.0) * pi * raio * raio * raio;
     return volume;
 }
