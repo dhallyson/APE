cdf4cc96f22ab153d1a7e5e250943b9626a9bf70
Dhallyson Santos <39205621+dhallyson@users.noreply.github.com>
2023-10-31 10:32:45 -0300

    Create exercicio06.c

---
+++ IntroducaoFuncoes/exercicio06.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+double calcularIMC(double peso, double altura);
+
+int main() {
+    double peso, altura, imc;
+    
+    printf("Digite o peso em quilos: ");
+    scanf("%lf", &peso);
+
+    printf("Digite a altura em metros: ");
+    scanf("%lf", &altura);
+
+    imc = calcularIMC(peso, altura);
+
+    printf("O IMC é: %.2lf\n", imc);
+
+    return 0;
+}
+
+double calcularIMC(double peso, double altura) {
+    double imc = peso / (altura * altura);
+    return imc;
+}
