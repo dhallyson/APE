a0172d4bd5909ec197a6759ab70ca6b01046b2ad
Dhallyson Santos <39205621+dhallyson@users.noreply.github.com>
2023-11-14 16:28:48 -0300

    Update atividade01.c

--- AtividadeAvaliativa2.0/atividade01.c
+++ AtividadeAvaliativa2.0/atividade01.c
@@ -2,53 +2,43 @@
 
 int validaQuantidade(int numeroPecas);
 float calculaSalario(int numeroPecas);
-void mostraFinal(float numeroPecas);
+void mostraFinal(int numeroPecas);
 
-main(void) {
-  float salarioBase = 600, salarioFinal;
-  int numeroPecas, calc50, calc30;
+int main(void) {
+  float salarioBase = 600.00;
+  int numeroPecas;
 
-  printf("Digite a quantidade de peças fabricadas: ");
-  scanf("%d", &numeroPecas);
-  
-  mostraFinal(numeroPecas);
+  do {
+    printf("Digite a quantidade de peças fabricadas: ");
+    scanf("%d", &numeroPecas);
+    mostraFinal(numeroPecas);
+  } while (numeroPecas >= 0);
+
+  return 0;
 }
 
-int validaQuantidade(int numeroPecas){
-  if(numeroPecas >= 0){
-    return(numeroPecas);
-  }else{
+int validaQuantidade(int numeroPecas) {
+  if (numeroPecas >= 0) {
+    return numeroPecas;
+  } else {
     return 1;
   }
 }
 
-float calculaSalario(int numeroPecas){
-  float salarioFinal;
-
-  if(numeroPecas < 50){
-    return salarioFinal = 600.00;
-
-  }else if(numeroPecas >= 50 && numeroPecas <= 80){
-
-    int calc50 = numeroPecas - 50;
-    float salarioFinal = 600.00 + calc50 * 0.50;
-    return salarioFinal;
-
-  }else if(numeroPecas > 80){
-
-    float calc80 = (numeroPecas - 80) * 0.75;
-    float salarioFinal = calc80 + 600 + 15.50;
-    return salarioFinal;
-
+float calculaSalario(int numeroPecas) {
+  if (numeroPecas < 50) {
+    return 600.00;
+  } else if (numeroPecas <= 80) {
+    return 600.00 + (numeroPecas - 50) * 0.50;
+  } else {
+    return (numeroPecas - 80) * 0.75 + 600.00 + 15.00;
   }
 }
 
-void mostraFinal(float numeroPecas){
-
-  if(validaQuantidade(numeroPecas) == 1){
-    printf("Quantidade inválida");
-  }else{
-    printf("O salário final é: %.2f", calculaSalario(numeroPecas));
+void mostraFinal(int numeroPecas) {
+  if (validaQuantidade(numeroPecas) == 1) {
+    printf("Quantidade inválida\n");
+  } else {
+    printf("O salário final é: %.2f\n\n", calculaSalario(numeroPecas));
   }
 }
-
