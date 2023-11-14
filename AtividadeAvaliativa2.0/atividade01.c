87a669af0c9a11b9c613ed79c05de7437ac062a0
Dhallyson Santos <39205621+dhallyson@users.noreply.github.com>
2023-11-14 11:12:28 -0300

    Update atividade01.c

--- AtividadeAvaliativa2.0/atividade01.c
+++ AtividadeAvaliativa2.0/atividade01.c
@@ -1,42 +1,8 @@
 #include <stdio.h>
 
-int validaQuantidade(int numeroPecas){
-  if(numeroPecas >= 0){
-    return(numeroPecas);
-  }else{
-    return 1;
-  }
-}
-
-float calculaSalario(int numeroPecas){
-  float salarioFinal;
-  
-  if(numeroPecas < 50){
-    return salarioFinal = 600.00;
-    
-  }else if(numeroPecas >= 50 && numeroPecas <= 80){
-    
-    int calc50 = numeroPecas - 50;
-    float salarioFinal = 600.00 + calc50 * 0.50;
-    return salarioFinal;
-    
-  }else if(numeroPecas > 80){
-    
-    float calc80 = (numeroPecas - 80) * 0.75;
-    float salarioFinal = calc80 + 600 + 15.50;
-    return salarioFinal;
-    
-  }
-}
-
-mostraFinal(float numeroPecas){
-  
-  if(validaQuantidade(numeroPecas) == 1){
-    printf("Quantidade inválida");
-  }else{
-    printf("O salário final é: %.2f", calculaSalario(numeroPecas));
-  }
-}
+int validaQuantidade(int numeroPecas);
+float calculaSalario(int numeroPecas);
+void mostraFinal(float numeroPecas);
 
 main(void) {
   float salarioBase = 600, salarioFinal;
@@ -48,4 +14,41 @@ main(void) {
   mostraFinal(numeroPecas);
 }
 
+int validaQuantidade(int numeroPecas){
+  if(numeroPecas >= 0){
+    return(numeroPecas);
+  }else{
+    return 1;
+  }
+}
+
+float calculaSalario(int numeroPecas){
+  float salarioFinal;
+
+  if(numeroPecas < 50){
+    return salarioFinal = 600.00;
+
+  }else if(numeroPecas >= 50 && numeroPecas <= 80){
+
+    int calc50 = numeroPecas - 50;
+    float salarioFinal = 600.00 + calc50 * 0.50;
+    return salarioFinal;
+
+  }else if(numeroPecas > 80){
+
+    float calc80 = (numeroPecas - 80) * 0.75;
+    float salarioFinal = calc80 + 600 + 15.50;
+    return salarioFinal;
+
+  }
+}
+
+void mostraFinal(float numeroPecas){
+
+  if(validaQuantidade(numeroPecas) == 1){
+    printf("Quantidade inválida");
+  }else{
+    printf("O salário final é: %.2f", calculaSalario(numeroPecas));
+  }
+}
 
